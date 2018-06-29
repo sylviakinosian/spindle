9e1847cb15a0a51dedd88431a1cfc2190ec61baf
Sylvia <sylvia.kinosian@gmail.com>
2018-06-29 12:38:05 -0400

    added fields

--- src/app/species-form/species-form.component.ts
+++ src/app/species-form/species-form.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 
-import { Species }    from '../species';
+import { Species } from '../species';
 
 @Component({
   selector: 'app-species-form',
   templateUrl: './species-form.component.html',
   styleUrls: ['./species-form.component.css']
 })
-export class SpeciesFormComponent {
 
-  model = new Species('Pteridaceae', 'Ceratopteris', 'thalictroides')
+export class SpeciesFormComponent {
 
   submitted = false;
 
   onSubmit() { this.submitted = true; }
-
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
 }
--- src/app/species.ts
+++ src/app/species.ts
@@ -3,7 +3,15 @@ export class Species {
   constructor(
     public family: string,
     public genus: string,
-    public species: string
+    public species: string,
+    public collector: string,
+    public colNumber: number,
+    public date: string,
+    public location: string,
+    public elevation?: string,
+    public coordinates?: string,
+    public otherSpecies?: string,
+    public notes?: string
   ) {  }
 
 }
