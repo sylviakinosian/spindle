416a09333cd4a43c803a7c07ff2d8bcfe03e6828
Sylvia <sylvia.kinosian@gmail.com>
2018-08-30 11:36:56 -0600

    new approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ngCsv } from '../../node_modules/ng-csv';
 
 import { AppComponent } from './app.component';
 
